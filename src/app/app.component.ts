8c6e8f5e8f286dadae1b175609caf4127d601354
qadeveloper <info+dev@virginmobilecolombia.com>
2019-08-20 15:10:36 -0500

    calculadora angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { identifierModuleUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+  var1=0;
+  var2=0;
+  result =0;
+  nombre:string='';
+
+  suma(){
+
+    this.result=this.var1+this.var2;
+    
+  }
+
+  resta(){
+
+    this.result=this.var1-this.var2;
+    
+
+  }
+
+  multiplicacion(){
+
+    this.result=this.var1*this.var2;
+   
+
+  }
+
+  division(){
+    if(this.var2==0){  
+      alert("No se puede dividir entre cero");
+    }else{
+      
+      this.result=(this.var1/this.var2);
+    }
+  }
+
+  modulo(){
+
+    this.result=this.var1%this.var2;
+
+  }
+
+  reset(){
+
+    this.var1=0;
+    this.var2=0;  
+    this.result=0;
+  }
+
+
+
+
 }
